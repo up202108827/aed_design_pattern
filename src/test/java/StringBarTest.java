a082aaf17d7b507e7dd29871c6cd8f74843ba8d6
Nuno Silva <nunosilva@MacBook-Pro-de-Nuno-4.local>
2022-11-15 11:54:46 +0000

    Final commit

--- src/test/java/StringBarTest.java
+++ src/test/java/StringBarTest.java
@@ -76,4 +76,39 @@ public class StringBarTest {
         assertEquals("dCbX-DcBa", drink.getText());
 
     }
+    @Test
+    public void ferengiAlreadyOpened() {
+        StringBar stringBar = new StringBar();
+        StringDrink drink = new StringDrink("AbCd-aBcD");
+        StringRecipe recipe = getRecipe();
+        FerengiClient client = new FerengiClient();
+        // Recipe is ordered immediately
+        stringBar.startHappyHour();
+        client.wants(drink, recipe, stringBar);
+        assertEquals("dCbX-DcBa", drink.getText());
+    }
+
+    @Test
+    public void ferengiStartClosed() {
+        StringBar stringBar = new StringBar();
+        StringDrink drink = new StringDrink("AbCd-aBcD");
+        StringRecipe recipe = getRecipe();
+        FerengiClient client = new FerengiClient();
+        stringBar.addObserver(client);
+        client.wants(drink, recipe, stringBar);
+        assertEquals("AbCd-aBcD", drink.getText());
+        // Recipe is only ordered here
+        stringBar.startHappyHour();
+        assertEquals("dCbX-DcBa", drink.getText());
+    }
+    @Test
+    public void romulan() {
+        StringBar stringBar = new StringBar();
+        StringDrink drink = new StringDrink("AbCd-aBcD");
+        StringRecipe recipe = getRecipe();
+        RomulanClient client = new RomulanClient();
+        // Recipe is ordered immediately
+        client.wants(drink, recipe, stringBar);
+        assertEquals("dCbX-DcBa", drink.getText());
+    }
 }
